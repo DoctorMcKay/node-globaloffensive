e78fa5721e9fa30d2250556d18e25bc544810107
Alexander Corn <mckay@doctormckay.com>
2017-10-27 01:16:06 -0400

    Don't poison the global Protobuf namespace with convertFieldsToCamelCase

--- protos.js
+++ protos.js
@@ -1,9 +1,6 @@
-var fs = require('fs');
 var Protobuf = require('protobufjs');
 
-Protobuf.convertFieldsToCamelCase = true;
-
-var builder = Protobuf.newBuilder();
+var builder = Protobuf.newBuilder({"convertFieldsToCamelCase": true});
 Protobuf.loadProtoFile(__dirname + '/protos/base_gcmessages.proto', builder);
 Protobuf.loadProtoFile(__dirname + '/protos/gcsdk_gcmessages.proto', builder);
 Protobuf.loadProtoFile(__dirname + '/protos/cstrike15_gcmessages.proto', builder);
